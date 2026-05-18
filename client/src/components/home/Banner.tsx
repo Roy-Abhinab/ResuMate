3c70995e82439c7cabd0483a8e7acc56b63b2b7b
Abhinab Roy <133621842+Roy-Abhinab@users.noreply.github.com>
2026-05-18 22:21:18 +0530

    Added some prebuilt UIs for Home page

--- client/src/components/home/Banner.tsx
+++ client/src/components/home/Banner.tsx
@@ -1,10 +1,10 @@
 
 const Banner = () => {
-  return (
-   <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-linear-to-r from-[#ABFF7E] to-[#FDFEFF]">
+   return (
+      <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-linear-to-r from-[#ABFF7E] to-[#FDFEFF]">
          <p><span className="px-3 py-1 rounded-lg text-white bg-green-600 mr-2">New</span>AI Feature Added</p>
-   </div>   
-  )
+      </div>
+   )
 }
 
 export default Banner
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import Banner from "../components/home/Banner"
+import Features from "../components/home/Features"
+import Hero from "../components/home/Hero"
 
 const Home = () => {
-  return (
-    <div>
-      <Banner />
-    </div>
-  )
+   return (
+      <div>
+         <Banner />
+         <Hero />
+         <Features />
+      </div>
+   )
 }
 
 export default Home
